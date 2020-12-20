a8401efe5e45fda873d266c1f0f9af4985276f11
Vasco <vaskopm@gmail.com>
2020-12-20 18:30:47 +0000

    Changes in source code
    Use of Github APIs

--- scangit.py
+++ scangit.py
@@ -9,52 +9,67 @@ import getpass
 import shutil
 from sys import platform
 
+#Initial data
+username = input("Github username: ")
+repo = input("Repository name: ")
+sendEmail = int(input("Send results by email? 1 - Yes; 2 - No: "))
+
 #Create input folder
 inputFolder = "InputFolder"
 if not (os.path.exists(inputFolder)):
     os.mkdir(inputFolder)
-    print(inputFolder + "Created!")
 
 #Get zip folder from gitub and extract it
-zipUrl = input("Zip repository url download: ")
+print("Downloading Repo...")
+zipUrl = "https://api.github.com/repos/" + username + "/" + repo + "/zipball"
 req = requests.get(zipUrl, stream=True)
 zip = ZipFile(io.BytesIO(req.content))
 zip.extractall(inputFolder)
 print('Files extracted!')
 
 #Run cloc
-file_result = "result.txt"
-if platform == "win32":
-    os.system("cloc.exe InputFolder --out=" + file_result)
+if sendEmail==1:
+    print("Reading and generating file result...")
+    file_result = "result.txt"
+    if platform == "win32":
+        os.system("cloc.exe InputFolder --out=" + file_result)
+    else:
+        os.system("cloc InputFolder --out=" + file_result)
+
+    #Send email with result file
+    sender = 'xxxxxxxxx' #sender = email sender
+    serverPassw = "xxxxxxxxx" #serverPassw = email server/sender password
+    receiver = input("Your email: ") 
+    print("Sending email...")
+
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587) #server = smtplib.SMTP('your-smtp-server', port-number)
+        message = MIMEMultipart()
+        message["From"] = sender
+        message["To"] = receiver
+        message["Subject"] = "Cloc Results"
+        message.attach(MIMEText("We are sending you an attachment file with your cloc results.", "plain"))
+        with open(file_result, "rb") as attachment:
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(attachment.read())
+        encoders.encode_base64(part)
+        part.add_header("Content-Disposition",f"attachment; filename= {file_result}")
+        message.attach(part)
+        text = message.as_string()
+        server.starttls()
+        server.login(sender, serverPassw)
+        server.sendmail(sender, receiver, text)
+        server.quit()
+        print("Successfully sent email")
+    except SMTPException:
+        print ("Error: unable to send email")
+
 else:
-    os.system("cloc InputFolder --out=" + file_result)
-
-#Send email with result file
-sender = 'sender-email'
-serverPassw = getpass.getpass("SMTP Server password: ")
-receiver = input("Receiver email: ")
-
-try:
-    server = smtplib.SMTP('your-smtp-server', 587) #587 - port number
-    message = MIMEMultipart()
-    message["From"] = sender
-    message["To"] = receiver
-    message["Subject"] = "Cloc Results"
-    message.attach(MIMEText("We are sending you an attachment file with your cloc results.", "plain"))
-    with open(file_result, "rb") as attachment:
-        part = MIMEBase("application", "octet-stream")
-        part.set_payload(attachment.read())
-    encoders.encode_base64(part)
-    part.add_header("Content-Disposition",f"attachment; filename= {file_result}")
-    message.attach(part)
-    text = message.as_string()
-    server.starttls()
-    server.login(sender, serverPassw)
-    server.sendmail(sender, receiver, text)
-    server.quit()
-    print("Successfully sent email")
-except SMTPException:
-   print ("Error: unable to send email")
+    print("Reading files...")
+    if platform == "win32":
+        os.system("cloc.exe InputFolder")
+    else:
+        os.system("cloc InputFolder")
 
 #Delete input folder
 shutil.rmtree(inputFolder)
